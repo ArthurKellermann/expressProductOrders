b45229ffac9286bac084b7f83b0ea165c7f73a92
Arthur Kellermann <arthur.kellermann956@gmail.com>
2023-07-07 15:47:24 -0300

    updated dotenv

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const orders_routes = require('./src/routes/orders').orders;
 const users_routes = require('./src/routes/users').users;
 
 //Database Connection
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.emit('DB Connected');
     }).catch(e => console.log('Error: ' + e));
